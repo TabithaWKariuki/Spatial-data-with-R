289ed4776d418348b45047b986997aa607d36b3f
pdtyrrell <peterdavidtyrrell@gmail.com>
2022-08-25 16:02:42 +0300

    Edits to OSM function

--- OSM_function.R
+++ OSM_function.R
@@ -1,30 +1,35 @@
 
-
 library(sf)
 library(osmdata)
 library(dplyr)
 library(ggplot2)
 library(ggmap)
 library(leaflet)
+library(stringr)
 
 
-get_roads <- function(city){
+get_roads <- function(city, 
+                      value = c("primary", "secondary", "trunk")){
+  
+
+  value <-if(any(str_detect(value,"All"))){NULL} else {value}
+  
   get_streets <- getbb(city)%>%
     opq()%>%
     add_osm_feature(key = "highway",
-                    value = c("primary", "secondary")) %>%
+                    value = value) %>%
     osmdata_sf()
   
   
-  rbind(
+allroads<- rbind(
     get_streets$osm_lines,
     get_streets$osm_multilines
   )
   
-  return(get_streets)
+  return(allroads)
 }
 
 
+nairobi_roads <- get_roads('Nairobi', value = c("All"))
 
-get_roads('Nairobi')
-
+mapview::mapview(nairobi_roads)
